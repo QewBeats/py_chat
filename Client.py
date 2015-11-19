30d00c752781bd0a7f5433b89b401c14627ed0de
QewBeats <quinzebm@gmail.com>
2015-11-19 18:51:24 +0000

    1.created a class for the server using static methods and variables(since they'd be accessed only)
    2.created a class for the client , to be initialized with every client object created
    
    3.Tested with two clients

--- Client.py
+++ Client.py
@@ -1,16 +1,26 @@
 import zmq
-import sys
 
-context = zmq.Context()
-print ("Connecting  to server")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://127.0.0.1:2000")
 
-for request in range(1,10):
-    print("Sending Request ", request)
-    socket.send_string("hello")
 
-    #Get reply
-    message = socket.recv()
-    print("Server says ", message)
+class Client():
+
+
+
+        def __init__(self):
+            self.context = zmq.Context()
+            self.socket =self.context.socket(zmq.REQ)
+
+
+        def connect(self):
+            self.socket.connect("tcp://127.0.0.1:2000")
+
+        def send_message(self,message):
+            self.socket.send_string(message)
+
+
+        def recv_message(self):
+            return self.socket.recv_string()
+
+
+
 
