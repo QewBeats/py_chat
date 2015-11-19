eab0ed7ce6d63077e3c2f079b47e23fe151826e1
Philemon Hini <philemonhini@gmail.com>
2015-11-19 08:45:48 -0500

    Client.py  will communicate with the server

--- Client.py
+++ Client.py
@@ -8,7 +8,7 @@ socket.connect("tcp://127.0.0.1:2000")
 
 for request in range(1,10):
     print("Sending Request ", request)
-    socket.send("hello")
+    socket.send_string("hello")
 
     #Get reply
     message = socket.recv()
